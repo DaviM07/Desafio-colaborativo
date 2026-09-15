25a8d165f75c551dfad81942971790630a0e849f
LUAN DOS SANTOS RODRIGUES <luan.rodrigues007@cs.udf.edu.br>
2026-09-15 10:58:47 -0300

    Defs Soma e Multi

---
+++ def.py
@@ -0,0 +1,15 @@
+def soma():
+      
+    n1=float(input("Digite um numero: "))
+    n2=float(input("Digite um numero: "))
+    soma=n1+n2
+    print(f"Resultado é {soma}")
+
+def multiplicacao():
+    n1=float(input("Digite um numero: "))
+    n2=float(input("Digite um numero: "))
+    multi=n1*n2
+    print(f"Resultado é {multi}")
+
+soma()
+multiplicacao()
