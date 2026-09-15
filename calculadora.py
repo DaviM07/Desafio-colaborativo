94f908cef96a11c37418b329a44deb85823d1ca5
DAVI DE ARAUJO MOURA DA SILVA <davi.silva2007@cs.udf.edu.br>
2026-09-15 10:10:32 -0300

    Commit soma

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+print("###Calculadora###")
+
+opcao = int(input("Escolha uma opção:")) 
+
+if opcao == 1:
+    print("soma")
+
+    n1 = float(input("Digite o primeiro numero: "))
+    n2 = float(input("Digite o segundo numero: "))
+
+soma = n1 + n2
+print(soma)
