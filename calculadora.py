f542ad6f59953315c2cb53c2c81c43b2ce5d781c
daniel <daniel.inazumaeleven@gmail.com>
2026-09-15 10:14:16 -0300

    multiplicação pronta

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+# Função de multiplicação
+def multiplicacao(a, b):
+    return a * b
+
+
+# Programa principal
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+resultado = multiplicacao(numero1, numero2)
+
+print(f"Resultado: {resultado}")
